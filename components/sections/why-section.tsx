76143ee9a1eef073f0c01e2313bf945a01d269f9
yegoshua <ses040914@gmail.com>
2026-01-24 16:19:19 +0100

    scroll animation vh update

--- components/sections/why-section.tsx
+++ components/sections/why-section.tsx
@@ -42,7 +42,7 @@ export function WhySection() {
       <div
         ref={containerRef}
         className="relative bg-coral rounded-section"
-        style={{ height: "1200vh" }}
+        style={{ height: "1000vh" }}
       >
         <div className="sticky top-0 h-screen overflow-hidden">
           <div className="container-main relative z-10 h-full flex flex-col py-16 lg:py-24">
